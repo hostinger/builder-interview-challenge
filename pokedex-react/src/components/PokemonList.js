ada3bfc40bae76c32965e948f637dcd0bf438f25
domnantas <domantas@zyro.com>
2021-02-12 02:10:00 +0200

    Remove react-router

--- pokedex-react/src/components/PokemonList.js
+++ pokedex-react/src/components/PokemonList.js
@@ -1,17 +1,12 @@
-import { onMount, useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import styles from "./PokemonList.module.css";
 
 const PokemonList = () => {
 	const allPokemon = useState(null);
 
-	// onMount(() => {
-	// 	Fetch pokemon
-	// });
-
 	return (
 		<div className={styles.list}>
-			<Link to="/pokemon/name" className={styles.card}>
+			<div className={styles.card}>
 				<img className={styles.image} />
 				<h2 className={styles.name}></h2>
 				{/* <svg
@@ -21,13 +16,13 @@ const PokemonList = () => {
 				className={styles.star}
 			>
 				<polygon
-					fill={pokemon.favorite ? "#eee" : "none"}
-					stroke="#eee"
+					fill={pokemon.favorite ? "#fff" : "none"}
+					stroke="#fff"
 					stroke-width="20"
 					points="150,25 179,111 269,111 197,165  223,251 150,200 77,251 103,165 31,111 121,111"
 				/>
 			</svg> */}
-			</Link>
+			</div>
 		</div>
 	);
 };
